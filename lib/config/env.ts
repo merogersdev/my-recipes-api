d6d799ae4c9a6384170a952bbfc3f822d59775af
Michelle Rogers <michelle@merogers.dev>
2024-08-27 16:45:04 -0600

    feature: change aws creds to use cli defaults

--- lib/config/env.ts
+++ lib/config/env.ts
@@ -4,8 +4,8 @@ dotenv.config();
 import type { EnvConfigType } from "../shared/types";
 
 export const getEnvConfig = (): EnvConfigType => ({
-  AWS_ACCOUNT: process.env.AWS_ACCOUNT || "",
-  AWS_REGION: process.env.AWS_REGION || "",
+  AWS_ACCOUNT: process.env.CDK_DEFAULT_ACCOUNT || "",
+  AWS_REGION: process.env.CDK_DEFAULT_REGION || "",
   AWS_TABLE_NAME: process.env.AWS_TABLE_NAME || "",
   AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME || "",
 });
